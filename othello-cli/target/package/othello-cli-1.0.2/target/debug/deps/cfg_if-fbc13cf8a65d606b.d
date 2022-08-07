935b4612d2d17beae05420e461591585d992e7be
Lord-Lelsers <48894200+LelsersLasers@users.noreply.github.com>
2022-08-06 22:19:26 -0700

    cargo publish for 1.0.2

---
+++ othello-cli/target/package/othello-cli-1.0.2/target/debug/deps/cfg_if-fbc13cf8a65d606b.d
@@ -0,0 +1,7 @@
+C:\Users\milla\Desktop\coding\othello-cli\othello-cli\target\package\othello-cli-1.0.2\target\debug\deps\cfg_if-fbc13cf8a65d606b.rmeta: C:\Users\milla\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\milla\Desktop\coding\othello-cli\othello-cli\target\package\othello-cli-1.0.2\target\debug\deps\libcfg_if-fbc13cf8a65d606b.rlib: C:\Users\milla\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\milla\Desktop\coding\othello-cli\othello-cli\target\package\othello-cli-1.0.2\target\debug\deps\cfg_if-fbc13cf8a65d606b.d: C:\Users\milla\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\milla\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs:
